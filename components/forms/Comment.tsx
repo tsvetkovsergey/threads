8aba0dbf589d890fb8144e55d18e907a54c145bb
Sergei Tsvetkov <tsvetkov2004@gmail.com>
2023-10-25 10:00:06 +0300

    specified thread validations

--- components/forms/Comment.tsx
+++ components/forms/Comment.tsx
@@ -78,6 +78,7 @@ export default function Comment({
                   {...field}
                 />
               </FormControl>
+              <FormMessage />
             </FormItem>
           )}
         />
